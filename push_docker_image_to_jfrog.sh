d526a0351b29ba6a7d31a92f58f3cb57f6af545a
Zachary Blackwood <zachary.blackwood@dtn.com>
2021-05-21 14:02:08 -0400

    Switch url

--- push_docker_image_to_jfrog.sh
+++ push_docker_image_to_jfrog.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 
 # This builds a docker image for use in bitbucket-pipelines, and pushes it to
-# cloud artifactory
-# If the environment variable bamboo_REPO isn't set, it default to pushing to the
-# dev repo. It puts the newly-created image tag to common/dags/SIA_IMAGE
+# cloud artifactory. The first argument is the tag
 
-#REPO="dtn-cr-dev.jfrog.io/dtn/dsci/pipelines"
-REPO="dtn-docker-dev-local.jfrog.io/dtn/dsci/pipelines"
+REPO="dtn-cr-dev.jfrog.io/dtn/dsci/pipelines"
 
 echo "REPO=$REPO"
 
-IMAGE="$REPO:latest"
+IMAGE="$REPO:$1"
 
 docker image build -t "$IMAGE" -f Dockerfile .
 
